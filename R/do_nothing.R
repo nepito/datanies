6f4191c67d2818b36ba6bf5441807c2a0b479d9f
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-09-03 00:33:40 -0700

    ✅ Pass test

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -7,6 +7,6 @@ infer_fields <- function(datos) {
   n_col <- ncol(datos)
   tipos <- sapply(datos, typeof)
   names(tipos) <- NULL
-  fields <- comprehenr::to_list(for (i in 1:n_col) list("name" = nombres[i], "type" = tipos[i]))
+  fields <- comprehenr::to_list(for (i in 1:n_col) list("name" = nombres[i], "type" = tipos[i], "standard_name" = ""))
   return(fields)
 }
