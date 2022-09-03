17a50db25b831b8ce4e96b29d4eab14c43f44cec
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-09-03 00:39:53 -0700

    ♻️ Refactor function `get_basic_field()`

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -7,6 +7,11 @@ infer_fields <- function(datos) {
   n_col <- ncol(datos)
   tipos <- sapply(datos, typeof)
   names(tipos) <- NULL
-  fields <- comprehenr::to_list(for (i in 1:n_col) list("name" = nombres[i], "type" = tipos[i], "standard_name" = ""))
+  fields <- comprehenr::to_list(for (i in 1:n_col) get_basic_field(nombres[i], tipos[i]))
   return(fields)
+}
+
+get_basic_field <- function(nombres = "", tipos = "", nombre_estandar = "") {
+  basic_field <- list("name" = nombres, "type" = tipos, "standard_name" = nombre_estandar)
+  return(basic_field)
 }
